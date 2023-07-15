2d8dcfd5328e83b4ce8f78ebf0b196e888c0e74b
DongWon Sehr <ddoro36@gmail.com>
2023-07-15 22:45:50 +0900

    + function calculate

--- src/ezDatetime.js
+++ src/ezDatetime.js
@@ -36,6 +36,37 @@ class ezDatetime {
   
       return formatOptions;
     }
+
+    calculate(operation) {
+      const match = operation.match(/([+-]?\d+) (\w+)/);
+      if (!match) {
+        throw new Error('Invalid operation format. Please use format like "+1 day" or "-3 months".');
+      }
+      const [, value, unit] = match;
+      const unitValue = this.getUnitValue(unit);
+      if (isNaN(unitValue)) {
+        throw new Error(`Invalid unit "${unit}".`);
+      }
+      const timeInMilliseconds = parseInt(value) * unitValue;
+      const newDate = new Date(this.date.getTime() + timeInMilliseconds);
+      this.date = new Date(newDate.toLocaleString('en-US', { timeZone: this.timezone }));
+      return this;
+    }
+
+    getUnitValue(unit) {
+      const unitMap = {
+        millisecond: 1,
+        second: 1000,
+        minute: 60 * 1000,
+        hour: 60 * 60 * 1000,
+        day: 24 * 60 * 60 * 1000,
+        week: 7 * 24 * 60 * 60 * 1000,
+        month: 30 * 24 * 60 * 60 * 1000,
+        year: 365 * 24 * 60 * 60 * 1000,
+      };
+  
+      return unitMap[unit.toLowerCase()] || NaN;
+    }
 }
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
